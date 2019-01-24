ab6f1d7d8ba6128d4323e61129c03fe8367adf4b
Jason Chen <jason@jcndrop.com>
2019-01-24 12:54:04 -0800

    Lock dependency versions

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,10 @@ description      "Installs/Configures Atlassian Crowd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.1"
 
-%w{database java postgresql}.each do |d|
-  depends d
-end
+depends 'database'
+depends 'java'
+depends 'postgresql', '~> 6.1.3'
+depends 'seven_zip', '< 3.0.0'
 
 %w{redhat centos}.each do |os|
   supports os
