9dd74695cae50fdc105079a5b05f60d07538febe
Andrew Dassonville <andrewda@osuosl.org>
2019-12-10 15:29:39 -0800

    Bump chef_version to >= 14.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ issues_url       'https://github.com/osuosl-cookbooks/crowd/issues'
 maintainer       "SecondMarket Labs, LLC"
 maintainer_email "systems@secondmarket.com"
 license          "Apache-2.0"
-chef_version     '>= 12.18' if respond_to?(:chef_version)
+chef_version     '>= 14.0'
 description      "Installs/Configures Atlassian Crowd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.0"
